9bb8c7c0d98395a9a8530157a14f459a9d2eb7a3
Calebe Zacai Costa <102423049+CalebeZacai@users.noreply.github.com>
2022-11-07 12:55:35 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double x = 10.35784;
+        String nome = "Maria";
+        int idade = 31;
+        double salario = 4000.0;
+        System.out.println(x);
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);
+        System.out.println("RESULTADO = " + x + " METROS");
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, salario);
+
     }
 }
