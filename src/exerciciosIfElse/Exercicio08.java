e7cc8257e54df77f5932a31c1d3c49b410b7467c
Calebe Zacai Costa <102423049+CalebeZacai@users.noreply.github.com>
2022-11-30 16:25:32 -0400

    Merge

--- src/exerciciosIfElse/Exercicio08.java
+++ src/exerciciosIfElse/Exercicio08.java
@@ -7,5 +7,25 @@ public class Exercicio08 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        double valor = sc.nextDouble();
+
+        double imposto;
+
+        if (valor <= 2000.0) {
+            imposto = 0;
+        } else if (valor <= 3000.0) {
+            imposto = (valor - 2000.0) * 0.08;
+        } else if (valor <= 4500.0) {
+            imposto = (valor - 3000.00) * 0.18 + 1000.0 * 0.08;
+        } else {
+            imposto = (valor - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+        } if (imposto == 0.00) {
+            System.out.println("Imposto de renda isento");
+        }  else {
+            System.out.printf("R$ %.2f%n", imposto);
+        }
+
+        sc.close();
     }
 }
