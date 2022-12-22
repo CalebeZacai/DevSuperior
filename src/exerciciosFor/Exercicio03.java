f8f988b796ae98a5340a778f5fbbc87d6c620bbe
Calebe Zacai Costa <102423049+CalebeZacai@users.noreply.github.com>
2022-12-22 13:37:47 -0400

    Initial commit

---
+++ src/exerciciosFor/Exercicio03.java
@@ -0,0 +1,7 @@
+package exerciciosFor;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/oop01/Program.java
@@ -0,0 +1,7 @@
+package oop01;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
