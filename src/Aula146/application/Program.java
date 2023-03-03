52a76200b2a43e2f557620e17a86e56789b1b670
Calebe Zacai Costa <102423049+CalebeZacai@users.noreply.github.com>
2023-03-03 12:43:43 -0400

    Commit 24/02/2023

---
+++ src/Aula146/application/Program.java
@@ -0,0 +1,13 @@
+package Aula146.application;
+
+import Aula146.entities.Order;
+import Aula146.entities.enums.OrderStatus;
+
+import java.util.Date;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+    }
+}
