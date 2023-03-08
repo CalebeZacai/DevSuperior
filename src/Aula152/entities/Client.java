8e3f1dabb03ecc35ec9c8e7828c4bee7c1d30f91
Calebe Zacai Costa <102423049+CalebeZacai@users.noreply.github.com>
2023-03-08 13:24:54 -0400

    08/03/2023

--- src/Aula152/entities/Client.java
+++ src/Aula152/entities/Client.java
@@ -1,4 +1,49 @@
 package Aula152.entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Client {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private String clientName;
+    private String clientEmail;
+    private Date clientBirthDate;
+
+    public Client(String clientName, String clientEmail, Date clientBirthDate) {
+        this.clientName = clientName;
+        this.clientEmail = clientEmail;
+        this.clientBirthDate = clientBirthDate;
+    }
+
+    public String getName() {
+        return clientName;
+    }
+
+    public void setName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getEmail() {
+        return clientEmail;
+    }
+
+    public void setEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    }
+
+    public Date getBirthDate() {
+        return clientBirthDate;
+    }
+
+    public void setBirthDate(Date clientBirthDate) {
+        this.clientBirthDate = clientBirthDate;
+    }
+
+    public String toString() {
+        return clientName
+                + " (" + sdf.format(clientBirthDate)
+                + ") - "
+                + clientEmail;
+    }
 }
--- src/Aula152/entities/Product.java
+++ src/Aula152/entities/Product.java
@@ -1,4 +1,28 @@
 package Aula152.entities;
 
 public class Product {
+
+    private String productName;
+    private Double productPrice;
+
+    public Product(String productName, Double productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
 }
