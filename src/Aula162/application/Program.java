4f258d495a33f8f080ae586cf1d91a90443faba7
Calebe Zacai Costa <102423049+CalebeZacai@users.noreply.github.com>
2023-04-27 12:56:48 -0400

    27/04/2023

--- src/Aula162/application/Program.java
+++ src/Aula162/application/Program.java
@@ -1,4 +1,51 @@
 package Aula162.application;
 
+import Aula162.entities.Product;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.println("Entre com o numero de produtos: ");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            System.out.println("Entre com o numero de produto #" );
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
